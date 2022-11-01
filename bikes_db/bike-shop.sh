b06b9b7178adb6ceaa33b2d17a484166e6bec101
Kevin Rietwyk <krietwyk@gmail.com>
2022-11-01 11:48:10 +1100

    Update bike-shop.sh

--- bikes_db/bike-shop.sh
+++ bikes_db/bike-shop.sh
@@ -12,7 +12,13 @@ MAIN_MENU() {
   echo "How may I help you?" 
   echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
-
+  
+  case $MAIN_MENU_SELECTION in 
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU "Please enter a valid option." ;;
+  esac
 }
 
 RENT_MENU() {
@@ -137,9 +143,3 @@ EXIT() {
 
 MAIN_MENU
 
-case $MAIN_MENU_SELECTION in 
-  1) RENT_MENU ;;
-  2) RETURN_MENU ;;
-  3) EXIT ;;
-  *) MAIN_MENU "Please enter a valid option." ;;
-esac
